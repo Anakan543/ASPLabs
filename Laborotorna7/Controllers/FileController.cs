22ab7b3af987af21a96460241ea1ed2edb195825
Anakan543 <117722031+Anakan543@users.noreply.github.com>
2024-10-17 12:45:20 +0300

    Add files via upload

---
+++ Laborotorna7/Controllers/FileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laborotorna7._1.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult FormURL()
+        {
+            return Redirect("/File/DownloadFile");
+        }
+        [HttpGet("/File/DownloadFile")]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IResult DownloadFile(string firstName, string secondName, string fileName)
+        {
+            string path = $"Files/{fileName}.txt";
+            string context = $"First name - {firstName}, Second name - {secondName}";
+
+            using (StreamWriter writer = new StreamWriter(path, append: false))
+            {
+                writer.WriteLine(context);
+            }
+            FileStream fileStream = new FileStream(path, FileMode.Open);
+            return Results.File(fileStream, "txt", $"{fileName}.txt");
+        }
+    }
+}
