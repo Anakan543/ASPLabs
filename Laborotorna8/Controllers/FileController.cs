f00b726e4684c68b5604f7af8570a0b033d02c73
Anakan543 <117722031+Anakan543@users.noreply.github.com>
2024-10-17 18:30:59 +0300

    Add files via upload

---
+++ Laborotorna8/Controllers/FileController.cs
@@ -0,0 +1,23 @@
+﻿using Laborotorna8.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Laborotorna8.Controllers
+{
+    public class FileController : Controller
+    {
+        private List<ProductModel> CreatedProducts()
+        {
+            return new List<ProductModel>() {
+                new ProductModel(0, "Product1", 4500), 
+                new ProductModel(1, "Product2", 7000),
+                new ProductModel(2, "Product3", 8000),
+                new ProductModel(3, "Product4", 1000)};
+        }
+        public IActionResult ReadItems()
+        {
+            List<ProductModel> products = CreatedProducts();
+            return View(products);
+        }
+    }
+}
---
+++ Laborotorna8/Models/ProductModel.cs
@@ -0,0 +1,43 @@
+﻿namespace Laborotorna8.Models
+{
+    public class ProductModel
+    {
+        private int ID;
+
+        private string name;
+
+        private int price;
+
+        private DateTime date = DateTime.Now;
+
+        public ProductModel(int ID, string name, int price) {
+        this.ID = ID;
+        this.name = name;
+        this.price = price;
+        }
+
+        public int GetID()
+        {
+            return ID;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetPrice()
+        {
+            return price;
+        }
+
+        public string GetData()
+        {
+            return date.ToString();
+        }
+        public override string ToString()
+        {
+            return $"{ID}.{name} - {price}({date.ToString()})";
+        }
+    }
+}
